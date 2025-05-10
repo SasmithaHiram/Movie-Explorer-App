ebe383eb0355aad9d84dc4df882d8697f2bad5d2
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-05-10 16:39:43 +0530

    Star Rating CSS Fix

--- src/components/movielist/MovieList.jsx
+++ src/components/movielist/MovieList.jsx
@@ -49,15 +49,33 @@ const MovieList = () => {
         <div className="align_center movie_list_fs">
           <ul className="align_center movie_filter">
             <li
-              className="movie_filter_item active"
+              className={
+                minRating === 8
+                  ? "movie_filter_item active"
+                  : "movie_filter_item"
+              }
               onClick={() => handleFilter(8)}
             >
               8+ Star
             </li>
-            <li className="movie_filter_item" onClick={() => handleFilter(7)}>
+            <li
+              className={
+                minRating === 7
+                  ? "movie_filter_item active"
+                  : "movie_filter_item"
+              }
+              onClick={() => handleFilter(7)}
+            >
               7+ Star
             </li>
-            <li className="movie_filter_item" onClick={() => handleFilter(6)}>
+            <li
+              className={
+                minRating === 6
+                  ? "movie_filter_item active"
+                  : "movie_filter_item"
+              }
+              onClick={() => handleFilter(6)}
+            >
               6+ Star
             </li>
           </ul>
